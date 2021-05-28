e3f6abcf7303c49a9d718baa6bb0bab7f0b307c5
Julia.Zorina <julia.zorinaa@gmail.com>
2021-05-28 08:50:32 +0300

    added resp menu

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,9 @@
 let addmenu = document.querySelector(".menu_img");
-let resp_menu =document.querySelector(".responsive_menu");
+let resp_menu = document.querySelector(".responsive_menu");
+let close_menu = document.querySelector(".menu_close");
+let menu_mobile = document.querySelector(".menu_mobile");
+let resp_menu_mob = document.querySelector(".resp_menu_mobile");
+let menu_closemob = document.querySelector(".menu_closemobile");
 addmenu.addEventListener("click", function(){
     if (resp_menu.classList.contains("_active")) {
         resp_menu.classList.remove("_active");
@@ -8,3 +12,26 @@ addmenu.addEventListener("click", function(){
 }
 });
 
+close_menu.addEventListener("click", function(){
+  if (resp_menu.classList.contains("_active")) {
+      resp_menu.classList.remove("_active");
+    } else {
+      resp_menu.classList.add("_active");
+}
+});
+
+menu_mobile.addEventListener("click", function(){
+  if (resp_menu_mob.classList.contains("_active")) {
+    resp_menu_mob.classList.remove("_active");
+    } else {
+      resp_menu_mob.classList.add("_active");
+}
+});
+
+menu_closemob.addEventListener("click", function(){
+  if (resp_menu_mob.classList.contains("_active")) {
+    resp_menu_mob.classList.remove("_active");
+    } else {
+      resp_menu_mob.classList.add("_active");
+}
+});
